d5568016a5a739d03435d9f10fe379b155bcc2f5
Thomas JEANNEAU <thomas.jeanneau@latechforce.com>
2026-01-26 17:37:18 +0100

    test: setup concurrency test files [skip ci]

---
+++ specs/test-concurrency/spec1.spec.ts
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) 2025 ESSENTIAL SERVICES
+ *
+ * This source code is licensed under the Business Source License 1.1
+ * found in the LICENSE.md file in the root directory of this source tree.
+ */
+
+import { test, expect } from 'bun:test'
+
+test.fixme('test in specs/test-concurrency/spec1.spec.ts', () => {
+  expect(1 + 1).toBe(2)
+})
