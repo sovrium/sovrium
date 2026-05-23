0dcdf82bd23611ed2e0e9f56d8b5796f7254a02b
Sovrium CI <ci@sovrium.com>
2026-05-23 23:07:30 +0000

    release: v0.7.0

--- src/infrastructure/css/generated-css-assets.ts
+++ src/infrastructure/css/generated-css-assets.ts
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) 2025-2026 ESSENTIAL SERVICES
- *
- * This source code is licensed under the Business Source License 1.1
- * found in the LICENSE.md file in the root directory of this source tree.
- */
-
 // @ts-nocheck -- generated file: huge embedded string literals, never type-checked
 /**
  * Copyright (c) 2025-2026 ESSENTIAL SERVICES
